e9b7da5d59f6b517827268843c2810b805bc1cd8
IronJon <lang.jeffrey@gmail.com>
2020-11-30 20:17:48 -0700

    adding file structure for node_modules and tests

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,46 +1,46 @@
 const Employee = require("../lib/Employee");
-
+//test to see if instance of employee creates an object
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
 });
-
+//test to see if instance of employee supplies name
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
-
+//tests to see if instance of employee supplies id
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
-
+//tests to see if instance of employee can supplies email
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
-
+//test to see that getName will return name value from employee instance
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
-
+//test to see that getId will return id value from employee instance
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
-
+//test to see that getEmail will return Email value from employee instance
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
-
+//test to see that getRole will return employee value from employee instance
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
